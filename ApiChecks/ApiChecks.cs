758e0a134c987d759df11b5937c1c525276566b7
imma <immasix@gmail.com>
2023-04-18 21:17:14 +0200

    first working check

---
+++ ApiChecks/ApiChecks.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System.Net;
+using System.Collections;
+using RestSharp;
+
+namespace ApiChecks
+{
+    [TestFixture]
+    public class ApiChecksClass
+    {
+        [Test]
+        public async Task VeryfyGetAllPizzasReturns200()
+        {
+            var options = new RestClientOptions("https://localhost:7292/pizza"){
+                RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+            };
+
+            var client = new RestClient(options);
+            var request = new RestRequest();
+
+            //Act
+            RestResponse response = await client.ExecuteGetAsync(request);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.Ok, response.StatusCode, $"Get all pizzas did not return a success status code; it returned {response.StatusCode}");
+        }
+    }
+}
