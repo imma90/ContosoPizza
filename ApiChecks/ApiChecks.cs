fee496eef0b08e4de68544302426b075018dd2f9
imma <immasix@gmail.com>
2023-04-19 19:11:18 +0200

    parameterized get by Id check

--- ApiChecks/ApiChecks.cs
+++ ApiChecks/ApiChecks.cs
@@ -27,11 +27,10 @@ namespace ApiChecks
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, $"Get all pizzas did not return a success status code; it returned {response.StatusCode}");
         }
 
-        [Test]
-        public async Task VerifyGetPizzaById1Returns200()
+        [Test, TestCaseSource(typeof(TestDataClass), "GetByIdTestData")]
+        public async Task<HttpStatusCode> VerifyGetPizzaByIdStatusCode(int id)
         {
             // Arrange
-            int id = 1;
             var options = new RestClientOptions($"https://localhost:7292/pizza/{id}"){
                 RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
             };
@@ -43,8 +42,7 @@ namespace ApiChecks
             RestResponse response = await client.ExecuteGetAsync(request);
 
             // Assert
-            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, $"Get pizzas with id {id} did not return a success status code; it returned {response.StatusCode}");
-
+            return response.StatusCode;
         }
 
         [Test]
@@ -73,6 +71,18 @@ namespace ApiChecks
             Assert.AreEqual(expectedPizza.Id, response.Data.Id,$"Get pizzas with id {expectedPizza.Id} did not return pizza with this id; it returned {response.Data.Id}");
             Assert.AreEqual(expectedPizza.Name, response.Data.Name,$"Actual name should have been {expectedPizza.Name} but it was {response.Data.Name}");
             Assert.AreEqual(expectedPizza.IsGlutenFree, response.Data.IsGlutenFree,$"Actual GlutenFree should have been {expectedPizza.IsGlutenFree} but it was {response.Data.IsGlutenFree}");
+        }
+    }
+
+    public class TestDataClass
+    {
+        public static IEnumerable GetByIdTestData
+        {
+            get
+            {
+                yield return new TestCaseData(1).Returns(HttpStatusCode.OK).SetName("id 1");
+                yield return new TestCaseData(39393).Returns(HttpStatusCode.NotFound).SetName("nonexistent id");
+            }
         }
     }
 }
